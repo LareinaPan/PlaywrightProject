8124b15f019d9c56c0d14f1a1b321ad27aa0adcd
yousi <1217198649@qq.com>
2026-05-19 13:49:39 +0800

    change3

--- test_example.py
+++ test_example.py
@@ -8,7 +8,7 @@ def test_has_title(page: Page):
     expect(page).to_have_title(re.compile("百度一下"))
 
 def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://playwright.dev/", timeout=30000)
     page.get_by_role("link", name="Get started").click()
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
 
